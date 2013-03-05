0d3de888cdf0948ab038e18439784b914fdf250c
sailorhero <sailorhero@163.com>
2013-03-06 02:00:13 +0800

    add bootstrap support,change routes defination and rspec_test

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
-    before {visit '/static_pages/home'}
+    before {visit root_path}
     it "should have_content 'Sample App'" do
       page.should have_content('Sample App')
     end
@@ -15,10 +15,25 @@ describe "StaticPages" do
       #page.should have_selector('title',
       #  :text => "Ruby on Rails Tutorial Sample App | Home")
     end
+
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      page.should have_selector 'h1', text: 'About Us'
+      click_link "Help"
+      page.should have_selector('h1','Help')
+      click_link "Contact"
+      page.should have_selector('h1','Contact')
+      click_link "Home"
+      click_link "Sign up now!"
+      page.should # fill in
+      click_link "sample app"
+      page.should have_selector('h1','Welcome to the Sample App')
+    end
   end
 
   describe "Help page" do
-    before {visit '/static_pages/help'}
+    before {visit help_path}
     it "should have_content 'Help'" do
       page.should have_content('Help')
     end
@@ -31,7 +46,7 @@ describe "StaticPages" do
   end
 
   describe "About page" do
-    before {visit '/static_pages/about'}
+    before {visit about_path}
     it "should have_content 'About Us'" do
       page.should have_content('About Us')
     end
@@ -40,6 +55,16 @@ describe "StaticPages" do
       find('title').native.text.should have_content("Ruby on Rails Tutorial Sample App | About Us")
       #page.should have_selector('title',
       #  :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe 'Contact page' do
+    before { visit contact_path }
+    it "should have_content 'Contact' " do
+      page.should have_selector('h1','Contract')
+    end
+    it "should have the title 'Contact'" do
+      find('title').native.text.should have_content("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
