3bb19412edac97502be46d7da629165f78bfae3f
sailorhero <sailorhero@163.com>
2013-03-07 22:33:07 +0800

    finish User model(including secure password)

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source 'http://ruby.taobao.org'
 
 gem 'rails', '3.2.12'
 gem 'bootstrap-sass'
+gem 'jquery-rails'
+
+gem 'bcrypt-ruby'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -50,7 +53,7 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
-gem 'jquery-rails'
+
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,24 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
-
-  validates_presence_of :name ,:email
-  validates_length_of :name, maximum:50
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-  validates_uniqueness_of :email, case_sensitive:false
+  attr_accessible :email, :name,:password,:password_confirmation
+  has_secure_password
 
   before_save {|user| user.email = email.downcase}
+
+  validates_presence_of :name ,:email,:password,:password_confirmation
+  validates_length_of :name, maximum:50
+  validates_length_of :password,minimum:6
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates_uniqueness_of :email, case_sensitive:false  
+  
 end
