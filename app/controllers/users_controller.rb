5cb12c9476e7c03026d81f33c1b76eec4b2b09f3
sailorhero <sailorhero@163.com>
2013-03-08 11:51:59 +0800

    finish user sign_up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
+# --encoding: utf-8 --
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #成功保存后的处理
+      flash[:success] = "欢迎登录Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -6,6 +6,27 @@ describe "UserPages" do
     before { visit signup_path}
     it { should have_selector('h1',text:'Sign Up')}
     it { find('title').native.text.should have_content(full_title("Sign Up"))}
+
+    let(:submit) {"Create my account"  }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect{ click_button submit}.not_to change(User,:count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",   with:"Example User"
+        fill_in "Email",  with: "user@example.com"
+        fill_in "Password",   with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect{click_button submit}.to change(User,:count).by(1)
+      end
+    end
   end
 
   describe "User show pages" do
